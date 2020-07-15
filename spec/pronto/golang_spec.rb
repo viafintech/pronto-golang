dbb5e2a08746525c2a25be7b8dfdd2cb6a82755a
Tobias Schoknecht <tobias.schoknecht@barzahlen.de>
2020-07-15 21:24:48 +0200

    Make tests less dependent on the exact order

--- spec/pronto/golang_spec.rb
+++ spec/pronto/golang_spec.rb
@@ -33,47 +33,60 @@ module Pronto
 
           aggregate_failures do
             # golangci-lint
-            expect(result[3].path).to eq('spec/fixtures/test.git/main.go')
-            expect(result[3].line.new_lineno).to eq(12)
-            expect(result[3].level).to eq(:warning)
-            expect(result[3].msg)
+            golangci = result.find { |r| r.msg =~ /^golangci-lint: S1000/ }
+
+            expect(golangci.path).to eq('spec/fixtures/test.git/main.go')
+            expect(golangci.line.new_lineno).to eq(12)
+            expect(golangci.level).to eq(:warning)
+            expect(golangci.msg)
               .to eq('golangci-lint: S1000: should use for range instead of for { select {} } (gosimple)')
-            expect(result[3].commit_sha).to eq('700d66789fa8a42b368fa890459b033e94d3216d')
-            expect(result[3].runner).to eq(Pronto::Golang)
+            expect(golangci.commit_sha).to eq('700d66789fa8a42b368fa890459b033e94d3216d')
+            expect(golangci.runner).to eq(Pronto::Golang)
 
             # golint
-            expect(result[8].path).to eq('spec/fixtures/test.git/main.go')
-            expect(result[8].line.new_lineno).to eq(21)
-            expect(result[8].level).to eq(:warning)
-            expect(result[8].msg)
+            golint = result.find { |r| r.msg =~ /^golint: exported/ }
+
+            expect(golint.path).to eq('spec/fixtures/test.git/main.go')
+            expect(golint.line.new_lineno).to eq(21)
+            expect(golint.level).to eq(:warning)
+            expect(golint.msg)
               .to eq('golint: exported function ExportedWithoutComment should have comment or be unexported')
-            expect(result[8].commit_sha).to eq('63d374bc2c05b2f5d8a1133b34d943f9da858542')
-            expect(result[8].runner).to eq(Pronto::Golang)
+            expect(golint.commit_sha).to eq('63d374bc2c05b2f5d8a1133b34d943f9da858542')
+            expect(golint.runner).to eq(Pronto::Golang)
+
             # gosec
-            expect(result[9].path).to eq('spec/fixtures/test.git/main.go')
-            expect(result[9].line.new_lineno).to eq(18)
-            expect(result[9].level).to eq(:warning)
-            expect(result[9].msg).to eq('gosec: G104: Errors unhandled. (Confidence: HIGH, Severity: LOW)')
-            expect(result[9].commit_sha).to eq('6456feb6134aee2a2615605274f7ed2d2d1ad84d')
-            expect(result[9].runner).to eq(Pronto::Golang)
+            gosec = result.find { |r| r.msg =~ /^gosec: G104/ }
+
+            expect(gosec.path).to eq('spec/fixtures/test.git/main.go')
+            expect(gosec.line.new_lineno).to eq(18)
+            expect(gosec.level).to eq(:warning)
+            expect(gosec.msg).to eq('gosec: G104: Errors unhandled. (Confidence: HIGH, Severity: LOW)')
+            expect(gosec.commit_sha).to eq('6456feb6134aee2a2615605274f7ed2d2d1ad84d')
+            expect(gosec.runner).to eq(Pronto::Golang)
+
             # go vet
-            expect(result[10].path).to eq('spec/fixtures/test.git/main.go')
-            expect(result[10].line.new_lineno).to eq(18)
-            expect(result[10].level).to eq(:warning)
-            expect(result[10].msg).to eq('go vet: unreachable code')
-            expect(result[10].commit_sha).to eq('6456feb6134aee2a2615605274f7ed2d2d1ad84d')
-            expect(result[10].runner).to eq(Pronto::Golang)
+            govet = result.find { |r| r.msg =~ /^go vet: / }
+
+            expect(govet.path).to eq('spec/fixtures/test.git/main.go')
+            expect(govet.line.new_lineno).to eq(18)
+            expect(govet.level).to eq(:warning)
+            expect(govet.msg).to eq('go vet: unreachable code')
+            expect(govet.commit_sha).to eq('6456feb6134aee2a2615605274f7ed2d2d1ad84d')
+            expect(govet.runner).to eq(Pronto::Golang)
+
             # staticcheck
-            expect(result[11].path).to eq('spec/fixtures/test.git/main.go')
-            expect(result[11].line.new_lineno).to eq(9)
-            expect(result[11].level).to eq(:warning)
-            expect(result[11].msg)
+            staticcheck = result.find { |r| r.msg =~ /^staticcheck:.+SA4017/ }
+
+            expect(staticcheck.path).to eq('spec/fixtures/test.git/main.go')
+            expect(staticcheck.line.new_lineno).to eq(9)
+            expect(staticcheck.level).to eq(:warning)
+            expect(staticcheck.msg)
               .to eq(
                 'staticcheck: ' \
                 'withUnusedParam is a pure function but its return value is ignored (SA4017)'
               )
-            expect(result[9].commit_sha).to eq('6456feb6134aee2a2615605274f7ed2d2d1ad84d')
-            expect(result[9].runner).to eq(Pronto::Golang)
+            expect(staticcheck.commit_sha).to eq('be3fb86b0177ab505c54104c7203c5f107053439')
+            expect(staticcheck.runner).to eq(Pronto::Golang)
           end
         end
       end
